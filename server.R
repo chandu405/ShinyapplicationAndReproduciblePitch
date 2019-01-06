bb405fea2101970659e5891c6cfa2043aec3f2f7
chandu405 <kanth.dugyala@gmail.com>
2019-01-06 08:37:33 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,98 +1,55 @@
 #
-# This is the server logic of a Shiny web application
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
 #
 
 library(shiny)
-library(ggplot2)
-
-# function for caluclating parabola
-calculateParabola <- function(x, stretchCompress, xIntercept, yIntercept) {
-   y <- stretchCompress * (xIntercept - x)^2 + yIntercept
-   data.frame(x=x, y=y)
-}
+library(UsingR)
 
 # Define server logic required to draw a histogram
-serverLogic <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      
-      xIntercept <- input$xIntercept
-      yIntercept <- input$yIntercept
-      stretchCompress <- input$stretchCompress
-      
-      normalParabola <- calculateParabola(seq(-10,10,by=0.1),
-                                          stretchCompress = 1,
-                                          xIntercept = 0,
-                                          yIntercept = 0)
-      
-      normalParabolaVertex <- calculateParabola(0,
-                                          stretchCompress = 1,
-                                          xIntercept = 0,
-                                          yIntercept = 0)
-      
-      parabola <- calculateParabola(seq(-10,10,by=0.1),
-                                    stretchCompress = stretchCompress,
-                                    xIntercept = xIntercept,
-                                    yIntercept = yIntercept)
-      
-      parabolaVertex <- calculateParabola(0+xIntercept,
-                                  stretchCompress = stretchCompress,
-                                  xIntercept = xIntercept,
-                                  yIntercept = yIntercept)
-      
-      # draw the histogram with the specified number of bins
-      ggplot() +
-         ggtitle(paste0("y = ",
-                        stretchCompress, "*(", 
-                        xIntercept, 
-                        "-x)^2+(", 
-                        yIntercept,
-                        ")")) + 
-         coord_cartesian(xlim = c(-10, 10), 
-                         ylim = c(-10, 10)) +
-         geom_line(data=normalParabola, aes(x=x, y=y), 
-                   size=2, 
-                   color="black", 
-                   alpha=0.3) + 
-         geom_vline(data=normalParabolaVertex, 
-                    aes(xintercept = x), 
-                    size=2
-                    , 
-                    color="black", 
-                    alpha=0.3) +
-         geom_hline(data=normalParabolaVertex, 
-                    aes(yintercept=y), 
-                    size=2, 
-                    color="black", 
-                    alpha=0.3) +
-         geom_point(data=normalParabolaVertex, 
-                    aes(x=x, y=y), 
-                    size=6, 
-                    shape = 4,
-                    color="black", 
-                    alpha=0.3) +
-         geom_line(data=parabola, 
-                   aes(x=x, y=y), 
-                   size=1,
-                   color="blue") + 
-         geom_vline(data=parabolaVertex, 
-                    aes(xintercept = x),
-                    colour = "red") +
-         geom_hline(data=parabolaVertex, 
-                    aes(yintercept=y),
-                    colour = "red") +
-         geom_point(data=parabolaVertex, 
-                    aes(x=x, y=y), 
-                    size = 5, 
-                    shape = 4,
-                    colour = "red")
-      
-   }, height = 300, width = 300)
-   
-}
 
-# Add serverLogic
-shinyServer(serverLogic)
+shinyServer(
+  
+  function(input, output) {
+    
+    k <- reactive({
+      
+      T<-input$T
+      h<-input$h
+      
+      w<-2*3.14159262/T
+      g =9.81
+      k0 <-0.025
+      
+      izq <- w^2/g/k0
+      dcha <- tanh(k0*h)
+      dif<- izq-dcha
+      
+      while (abs(dif) > 0.001) {
+        if(dif >0.01) {k0<-k0+.001
+        } else if(dif < -0.01) {k0<-k0-0.001
+        } else { break}
+        izq <- w^2/g/k0
+        dcha <- tanh(k0*h)
+        dif<- izq-dcha
+      }
+      
+      k<-k0 })
+    
+    output$waveplot <- renderPlot({
+      A<-input$A
+      h0<-input$h
+      T<-input$T
+      w<-2*3.14159262/T
+      L<-2*3.14159262/k()
+      Xmax <-1000
+      x<-seq(from=0, to = Xmax, by =L/100)
+      eta<-A*cos(k()*x)
+      plot(x,eta,ylab="Free Surface (m)", xlab="Direction of wave propagation (m)",col="blue",xlim=c(0,Xmax),ylim=c(-h0,A))
+    })
+    
+    output$k <-renderText({c(k(), "meters")})
+    
+  }
+)
